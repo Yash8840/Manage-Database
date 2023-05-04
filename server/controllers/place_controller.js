c5b1289fd97849ddbd81e160dbd192bb11b7465d
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-05-04 11:22:13 +0300

    place detail + list

--- server/controllers/place_controller.js
+++ server/controllers/place_controller.js
@@ -1,9 +1,21 @@
-exports.place_list = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: place list GET"); 
+const Place = require("../models/place");
+
+exports.place_list = async (req, res, next) => { 
+    const places = await Place.find({}, "title").exec(); 
+    if(places == null) { 
+        res.status(403); 
+    } else { 
+        res.status(200).json({ places }); 
+    }
 }; 
 
-exports.place_detail = (req, res, next) => { 
-    res.send(`NOT IMPLEMENTED: place detail ${req.params.id}`); 
+exports.place_detail = async (req, res, next) => { 
+    try { 
+        const place = await Place.findById(req.params.id).exec();
+        res.status(200).json({ place: place }); 
+    } catch (e){ 
+        res.status(403); 
+    }
 }; 
 
 exports.place_create_get = (req, res, next) => { 
