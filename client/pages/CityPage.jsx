41484080a7672abd5e0df3f7b3375131d3eda0c5
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-05-12 22:04:23 +0300

    See photos in detail for city

--- client/pages/CityPage.jsx
+++ client/pages/CityPage.jsx
@@ -4,15 +4,28 @@ import { useLocation } from "react-router-dom";
 import Place from "../components/Place";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import generateRandomKey from "../helpers/randomKey";
+import { Buffer } from "buffer"; 
 
 const CityPage = () => { 
     const [data, setData] = useState({}); 
     const [ placeTypes, setPlaceTypes ] = useState([]); 
-    const [placesData, setPlacesData] = useState({}); 
+    const [ placesData, setPlacesData ] = useState({}); 
     const location = useLocation(); 
     const navigate = useNavigate(); 
-
+    const [ images, setImages] = useState([]); 
     const [successDeleteMessage, setSuccessDeleteMessage] = useState(""); 
+
+    const formatImages = (array) => { 
+        const result = []; 
+            array.forEach(dataBuffer => { 
+                const base64image = Buffer.from(dataBuffer.data, 'base64').toString("base64"); 
+                result.push(`data:image/jpeg;base64,${base64image}`); 
+            }); 
+        
+
+        return result; 
+    }
 
     useEffect( () => {
         const renderData = async () => { 
@@ -20,10 +33,9 @@ const CityPage = () => {
                 const res = await fetchData(`http://localhost:3000/api/${location.pathname}`); 
                 const resTypes = await fetchData("http://localhost:3000/api/places/types"); 
                 setPlaceTypes(resTypes); 
-                console.log(resTypes); 
                 setData(res.city); 
-                setPlacesData(res.placesInCity); 
-                console.log(res.placesInCity); 
+                setPlacesData(res.placesInCity);
+                setImages(formatImages(res.city.photo.data)); 
             } catch(err) { 
                 console.log(err); 
                 console.log("in efect"); 
@@ -59,7 +71,6 @@ const CityPage = () => {
 
     return( 
         <section className="detail-page page">
-            <pre> { JSON.stringify(data) } </pre>
             <article className="developer-area">
                 <h2> { data.title } </h2>
                 <button onClick={ handleDelete }> Sterge Oras </button>
@@ -74,7 +85,14 @@ const CityPage = () => {
                     <p className="info components"> Urmatoarele sate apartin de acest oras: </p>
                     <p> { data.components } </p>
                 </>
-                }                
+                }         
+
+                {images.map(image => { 
+                    return ( 
+                        <img src= { image } alt="X" key={ generateRandomKey(20)} />
+                    )
+                })}
+                       
                 { data.history && 
                     <>
                         <p className="info history">Istoria orasului: </p>
@@ -83,8 +101,8 @@ const CityPage = () => {
                 }
 
                 <hr />
-                <p className="info population"> Populatie: { data.population } </p>
-                <p className="info surface"> Suprafata: { data.surface } km ^ 2  </p>
+                    <p className="info population"> Populatie: { data.population } </p>
+                    <p className="info surface"> Suprafata: { data.surface } km ^ 2  </p>
                 <hr />
 
                 <h3>Atractii: </h3>
