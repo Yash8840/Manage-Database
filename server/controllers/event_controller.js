9163758da610469dd08133776a6f0d3ea8dcfa32
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-05-14 21:35:55 +0300

    List detail create events - untested

--- server/controllers/event_controller.js
+++ server/controllers/event_controller.js
@@ -1,17 +1,58 @@
-exports.event_list = (req, res, next) => { 
-    res.send("Event List")
+const Event = require("../models/event"); 
+const upload = require("../middleware/upload_multer"); 
+
+exports.event_list = async (req, res, next) => { 
+    try {  
+        const events = await Event.find({}, { title: 1}).exec(); 
+        res.status(200).json({ events }); 
+    } catch(err) { 
+        console.log(err); 
+    }
 }; 
 
-exports.event_detail = (req, res) => { 
-    res.send(`Event detail: ${req.params.id}`); 
+exports.event_detail = async (req, res) => { 
+    try { 
+        const event = await Event.find(req.params.id)
+            .populate("places"); 
+        res.status(200).json({ event }); 
+    } catch(err) { 
+        console.log(err); 
+    }
+}; 
+
+exports.create_event_post = (req, res) => { 
+    upload(req, res, async(err) => { 
+        if(err) { 
+            console.log(err); 
+            console.log(res.body); 
+            console.log(res.files);  
+        } else { 
+            const dataFiles = []; 
+            if(req.files) { 
+                const files = req.files;
+                files.forEach(file => { 
+                    dataFiles.push(fs.readFileSync(`../uploads/${files[files.indexOf(file)].filename}`)); 
+                }); 
+            }
+
+            const placesDocs = req.body.places.split(","); 
+            console.log(placesDocs); 
+    
+            const newEvent = new Event({ 
+                title: req.body.title, 
+                description: req.body.description, 
+                photo: dataFiles, 
+                places: placesDocs, 
+            }); 
+
+            await newEvent.save(); 
+            res.status(200).json({ event: newEvent, successfull_message: "Event added successfully"}); 
+        }
+    })
 }; 
 
 exports.create_event_get = (req, res) => { 
     res.send("create event get"); 
-}; 
-
-exports.create_event_post = (req, res) => { 
-    res.send("create event post"); 
 }; 
 
 exports.delete_event = (req, res) => { 
