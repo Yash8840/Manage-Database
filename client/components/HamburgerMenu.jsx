b5c910e51c3a7a056127e2666e2bfae2af514ec3
Petrisorz <petrisorfrumuzache@gmail.com>
2023-05-16 15:16:59 +0300

    fixed Buttons

--- client/components/HamburgerMenu.jsx
+++ client/components/HamburgerMenu.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 function HamburgerMenu({isOpen}){
     return(
             <div className="hamburgerMenuList">
-                    <NavLink className="navbar-element navbar-burgerLink" to = "/places/create">Creeaza Atractie</NavLink> 
-                    <NavLink className="navbar-element navbar-burgerLink" to = "/cities/create"> Creeaza Oras </NavLink>    
-                    <NavLink className="navbar-element navbar-burgerLink" to = "/roads/create"> Creeaza Traseu </NavLink>          
-                    <NavLink className="navbar-element navbar-burgerLink" to = "/events/create"> Creeaza Eveniment </NavLink>          
+                    <NavLink className="navbar-element " to = "/places/create">Creeaza Atractie</NavLink> 
+                    <NavLink className="navbar-element " to = "/cities/create"> Creeaza Oras </NavLink>    
+                    <NavLink className="navbar-element " to = "/roads/create"> Creeaza Traseu </NavLink>          
+                    <NavLink className="navbar-element " to = "/events/create"> Creeaza Eveniment </NavLink>          
             <style>{`
             .hamburgerMenuList{
                 display:${isOpen ? `flex ` :`none`};
